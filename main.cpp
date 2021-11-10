a54f763eafcb16754783702b8d3aa0b5a21da39b
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2021-11-10 11:42:59 +0800

    Added command line arguments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <exception>
+#include <string>
+
 #include "tinyxml2/tinyxml2.h"
 #include "tclap/CmdLine.h"
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char* argv[]) {
+    try {
+        // Declare the command line.
+        TCLAP::CmdLine cmd("A program to convert musicxml to lilypond.", ' ', "beta");
+        
+        // Add the filename as mandatory
+        TCLAP::UnlabeledValueArg<std::string> filename_arg("filename",
+                                                           "The name of the file being converted",
+                                                           true,
+                                                           "",
+                                                           "string" );
+        
+        // Add the output name as optional
+        TCLAP::ValueArg<std::string> output_arg("o",
+                                                "output",
+                                                "The name of the file to output to: default is based on the input filename",
+                                                false,
+                                                "",
+                                                "string");
+        
+        // add those arguments
+        cmd.add(filename_arg);
+        cmd.add(output_arg);
+        
+        // Parse the command-line arguments.
+        cmd.parse(argc, argv);
+        std::string filename = filename_arg.getValue();
+        
+        std::cout << "Filename: " << filename << std::endl;
+    }
+    catch (TCLAP::ArgException &e) {
+        std::cerr << "Error: " << e.error() << " for arg " << e.argId() << std::endl;
+        exit(1);
+    }
+    catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        exit(2);
+    }
+    catch (...) {
+        std::cerr << "Unknown error occured. Terminating." << std::endl;
+        exit(3);
+    }
 }
