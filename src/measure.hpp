42827a62ca94fcf165db26f7ae34cd3c7d587656
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2021-12-04 12:05:31 +0800

    Revised measure definition to use abstract objects

--- src/measure.hpp
+++ src/measure.hpp
@@ -15,62 +15,22 @@
 #include <variant>
 #include <utility>
 
-#include "auxiliary.hpp"
+#include "measure_objects.hpp"
 #include "note.hpp"
-
-// ==> Forward declarations
-namespace lly {
-    // Defined elsewhere
-    class Note;
-    class Voice;
-    
-    // Defined here
-    class Measure;
-}
-
-std::ostream& operator<<(std::ostream&, const lly::Measure&);
+#include "measure_attributes.hpp"
+#include "measure_directions.hpp"
 
 // ==> Proper declarations
-namespace lly {
+namespace lmt {
     class Measure {
     public:
-        // ==> constructors
         Measure(int id_number);
-        Measure(int id_number, std::vector<lly::Note> notes);
         
-        // ==> Setters
-        void add_note(lly::Note);
-        void set_partial(); // makes it a partial measure. Does not change the time signature.
-        void set_newline(); // makes this measure on a new line.
-        void set_actual_time_signature(lly::aux::TimeSignature);
-        void add_measure_object(lly::aux::Clef);
-        void add_measure_object(lly::aux::KeySignature);
-        void add_measure_object(lly::aux::TimeSignature);
-        void add_measure_object(lly::aux::Barline);
-        void add_measure_object(lly::aux::MeasureTypes);
-        
-        // ==> Functions
-        // confirms that the measure has the correct number of notes
-        bool confirm();
-        
-        // ==> Friends
-        friend Voice;
-        friend std::ostream& ::operator<<(std::ostream&, const lly::Measure&);
+        void add_object(std::unique_ptr<lmt::aux::AbstractMeasureObject> abstract_ptr);
     private:
         // ==> Variables
-        std::vector<Note> notes;
         int id_number;
-        bool partial = false;
-        bool newline = false;
-        double count = 0;
-        std::optional<lly::aux::TimeSignature> actual_time_signature;
-        
-        std::optional<int> written_measure_number;
-        std::optional<lly::aux::Clef> written_clef;
-        std::optional<lly::aux::KeySignature> written_key_signature;
-        std::optional<lly::aux::TimeSignature> written_time_signature;
-        std::optional<lly::aux::Barline> left_barline;
-        std::optional<lly::aux::Barline> right_barline;
+        std::vector<std::unique_ptr<lmt::aux::AbstractMeasureObject>> objects;
     };
 }
 
