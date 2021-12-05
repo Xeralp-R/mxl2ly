f9d5964981a1b2f70fa22380a0c1d10672b364c8
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2021-12-05 11:07:35 +0800

    Added PartList class

--- src/part.cpp
+++ src/part.cpp
@@ -11,7 +11,16 @@ using namespace lmt;
 
 Part::Part(std::string id) : id(id) {}
 
+PartList::PartList(std::initializer_list<std::pair<std::string, std::string> > parts) {
+    for (auto& pair : parts) {
+        this->parts.insert(pair);
+    }
+}
+
+void PartList::add_part(std::string part_id, std::string part_name) { 
+    this->parts.insert(part_id, part_name);
+}
+
 void Part::add_measure(std::unique_ptr<Measure> measure_ptr) { 
     this->measures.push_back(std::move(measure_ptr));
 }
-
--- src/part.hpp
+++ src/part.hpp
@@ -12,11 +12,21 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <map>
 
 #include "statement.hpp"
 #include "measure.hpp"
 
 namespace lmt {
+    class PartList : public AbstractStatement {
+    public:
+        PartList(std::initializer_list<std::pair<std::string, std::string>> parts);
+        
+        void add_part(std::string part_id, std::string part_name);
+    private:
+        std::map<std::string, std::string> parts;
+    };
+    
     class Part : public AbstractStatement {
     public:
         Part(std::string id);
