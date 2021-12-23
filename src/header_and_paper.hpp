9a5a1f8c0fe55bd41bf588363e715937cab3613c
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2021-12-23 10:16:04 +0800

    Removed odd/even margin division
    Lilypond doesn't support odd/even margins, so I'm removing the functionality for them.
    Also styled the pages. Got clang-format to work on this machine, which is probably good.

--- src/header_and_paper.hpp
+++ src/header_and_paper.hpp
@@ -16,20 +16,21 @@
 #include <utility>
 #include <vector>
 
-#include "statement.hpp"
 #include "helper.hpp"
+#include "statement.hpp"
 
 namespace lmt {
     
     class Header : public AbstractStatement {
     public:
         Header() = default;
-        Header(std::initializer_list<std::pair<std::string, std::string>> statements);
+        Header(
+               std::initializer_list<std::pair<std::string, std::string>> statements);
         
         void add_statement(std::pair<std::string, std::string>);
         
         std::string get_type() override { return "header"; }
-        std::vector<Statement<std::string> *> get_statements();
+        std::vector<Statement<std::string>*> get_statements();
         
     private:
         std::vector<std::unique_ptr<Statement<std::string>>> statements;
@@ -37,37 +38,22 @@ namespace lmt {
     
     class Paper : public AbstractStatement {
     public:
-        enum class Margins {
-            EvenLeftMargin,
-            EvenRightMargin,
-            EvenTopMargin,
-            EvenBottomMargin,
-            OddLeftMargin,
-            OddRightMargin,
-            OddTopMargin,
-            OddBottomMargin
-        };
+        enum class Margins { LeftMargin, RightMargin, TopMargin, BottomMargin };
         
         Paper(Length height, Length width);
         
-        void set_even_margins(Length left_margin, Length right_margin,
-                              Length top_margin, Length bottom_margin);
-        
-        void set_odd_margins(Length left_margin, Length right_margin,
-                             Length top_margin, Length bottom_margin);
+        void set_margins(Length left_margin, Length right_margin, Length top_margin,
+                         Length bottom_margin);
         
         std::string get_type() override { return "paper"; }
         
-        Statement<Length> *get_margin(Paper::Margins margin);
+        Statement<Length>* get_margin(Paper::Margins margin);
         
     private:
         std::unique_ptr<Statement<Length>> height;
         std::unique_ptr<Statement<Length>> width;
         
-        using margin_array = std::array<std::unique_ptr<Statement<Length>>, 4>;
-        
-        margin_array even_margins;
-        margin_array odd_margins;
+        std::array<std::unique_ptr<Statement<Length>>, 4> margins;
     };
     
 } // namespace lmt
