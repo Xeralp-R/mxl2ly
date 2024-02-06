8a7235201a39fd12e8df0eedcfaeead78000c04b
Xer-Alp <80310657+Xeralp-R@users.noreply.github.com>
2024-02-06 11:55:14 +0800

    Updated to new highest version

--- src/music_tree.cpp
+++ src/music_tree.cpp
@@ -37,7 +37,7 @@ void MusicTree::print_lilypond(std::string filename) {
     std::cout << "Printing to: " << filename << '\n';
     this->out.open(filename);
     this->out << "% Automatically generated from a musicxml file." << newline;
-    this->out << R"-(\version "2.22.1")-" << newline << newline;
+    this->out << R"-(\version "2.24.0")-" << newline << newline;
 
     this->print_staff_info();
     this->print_paper_block();
